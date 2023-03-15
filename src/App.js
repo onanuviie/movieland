53650da05fb0baf936075f9e50efcceb71107cf0
onanuviie <nuviesilvet@gmail.com>
2023-03-15 12:45:45 +0100

    initial design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import SearchIcon from './search.svg'
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=1a40b53'
+
+const movie1 = {
+        "Title": "Italian Spiderman",
+        "Year": "2007",
+        "imdbID": "tt2705436",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
+    }
+
+
+const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
+
+        console.log(data.Search)
+    }
+ 
+    useEffect(() => {
+        searchMovies('SpiderMan')
+    }, [])
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for Movies"
+                    value="Superman"
+                    onChange={() => {}}
+                />
+                <img
+                    src = {SearchIcon}
+                    alt = "search"
+                    onClick={() => {}}
+                />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                <div>
+                    <p>{movie1.Year}</p>
+                </div>
+
+                <div>
+                    <img 
+                    src = {movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'}
+                    alt = {movie1.Title}/>
+                </div>
+                <div>
+                    <span>{movie1.Type}</span>
+                    <h3>{movie1.Title}</h3>
+                </div>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
